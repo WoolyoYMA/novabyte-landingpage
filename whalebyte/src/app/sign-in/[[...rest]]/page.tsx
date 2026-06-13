74eac2fc2c9111d19ea9f6682ee11e3531fa1d09
Pascal <deine-email@beispiel.de>
2026-06-13 17:33:16 +0200

    Revert mobile wrapper changes

--- whalebyte/src/app/sign-in/[[...rest]]/page.tsx
+++ whalebyte/src/app/sign-in/[[...rest]]/page.tsx
@@ -2,10 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#F7F7F7] p-4">
-      <div className="w-full max-w-md">
-        <SignIn />
-      </div>
+    <div className="min-h-screen flex items-center justify-center bg-[#F7F7F7]">
+      <SignIn />
     </div>
   );
 }
--- whalebyte/src/app/sign-up/[[...rest]]/page.tsx
+++ whalebyte/src/app/sign-up/[[...rest]]/page.tsx
@@ -2,10 +2,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#F7F7F7] p-4">
-      <div className="w-full max-w-md">
-        <SignUp />
-      </div>
+    <div className="min-h-screen flex items-center justify-center bg-[#F7F7F7]">
+      <SignUp />
     </div>
   );
 }
