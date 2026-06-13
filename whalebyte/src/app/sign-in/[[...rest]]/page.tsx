f11dac67f13608cfb7c450eef747d45936124ff0
Pascal <deine-email@beispiel.de>
2026-06-13 17:16:48 +0200

    Remove invalid redirectUrl props from SignIn/SignUp

--- whalebyte/src/app/sign-in/[[...rest]]/page.tsx
+++ whalebyte/src/app/sign-in/[[...rest]]/page.tsx
@@ -3,15 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#F7F7F7]">
-      <SignIn 
-        redirectUrl="/dashboard"
-        signUpUrl="/sign-up"
-        appearance={{
-          variables: {
-            colorPrimary: '#000000',
-          }
-        }}
-      />
+      <SignIn />
     </div>
   );
 }
--- whalebyte/src/app/sign-up/[[...rest]]/page.tsx
+++ whalebyte/src/app/sign-up/[[...rest]]/page.tsx
@@ -3,15 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#F7F7F7]">
-      <SignUp 
-        redirectUrl="/dashboard"
-        signInUrl="/sign-in"
-        appearance={{
-          variables: {
-            colorPrimary: '#000000',
-          }
-        }}
-      />
+      <SignUp />
     </div>
   );
 }
